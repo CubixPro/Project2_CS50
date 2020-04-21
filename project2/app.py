18d654ff5d33327963dd3ab7a1f328f3e73fdeb7
Titir Adhikary <titir.adhikary@hotmail.com>
2020-04-21 23:42:20 +0530

    Texting between two people

--- project2/app.py
+++ project2/app.py
@@ -1,13 +1,25 @@
 import os
+import requests
 
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+users = []
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    return render_template("login.html") 
+
+@app.route("/chat")
+def chat():
+    return render_template("chat.html")
+
+@socketio.on("message")
+def transmit(data):
+    message = data["message"]
+    print(message)
+    emit("transmit", {"message": message}, broadcast = True)
