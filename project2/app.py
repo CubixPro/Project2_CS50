cfe310f9d9c994cfac86c39bc9d6e9f8a4aa063f
Titir Adhikary <titir.adhikary@hotmail.com>
2020-04-26 12:56:01 +0530

    Messages show username Session can be created to remember login

--- project2/app.py
+++ project2/app.py
@@ -1,17 +1,22 @@
 import os
 import requests
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session
 from flask_socketio import SocketIO, emit, join_room
+from flask_session import Session
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+app.config["SESSION_PERMENENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 users = {} 
-messages = []
 channels = []
+messages = {}
 class User:
     def __init__(self, name):
         self.status = 'logged in'
@@ -21,12 +26,16 @@ class User:
 
 @app.route("/")
 def index():
-    return render_template("login.html") 
+    if session.get('username') != None:
+        return render_template("join.html", channel=channels, username=session['username'])
+    else:
+        return render_template("login.html") 
 
 @app.route("/enter", methods=["GET", "POST"])
 def enter():
     print("Entered")
     username = request.form.get("username")
+    session['username'] = username
     if users.get(username) == None :
         p =  User(username)
         users[username] = p 
@@ -46,16 +55,23 @@ def newchannel(username):
 def chat():
     roomname = request.form.get("channelname")
     print(roomname)
-    return render_template("chat.html", roomname = roomname)
+    if messages.get(roomname) == None:
+        messages[roomname] = {}
+    return render_template("chat.html", roomname = roomname, username=session['username'], messages=messages[roomname])
 
 @socketio.on("message")
 def transmit(data):
     message = data["message"]
     roomname = data["room"]
     #roomname="first_room"
-    print(message)
-    print(roomname)
+    #print(message)
+    #print(roomname)
+    #print(session['username'])
+    print(messages[roomname])
     join_room(roomname)
-    emit("transmit", {"message": message, "roomname": roomname}, room=roomname)
+    if messages.get(roomname) == None:
+        messages[roomname] = [] 
+    #messages[roomname].append(message)
+    emit("transmit", {"message": message, "roomname": roomname, "username": session['username']}, room=roomname)
 
 
